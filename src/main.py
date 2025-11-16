83207aaff2a331f94d31d080afe1128203f7513f
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2025-11-15 23:40:02 -0800

    updated transcribe script to support new output file names

--- src/main.py
+++ src/main.py
@@ -11,16 +11,22 @@ TEXT_OUT_DIR = os.getenv("TEXT_OUT_DIR")
 warnings.filterwarnings("ignore")
 model = Model("large-v3-turbo")
 
+regex = r".*[\/\\][A-Z]+0*([1-9][0-9]*)[^0-9]*"
+def get_number_str_from_path(file_path):
+    return re.match(regex, file_path).group(1)
+
 audio_paths = glob.glob(f"{AUDIO_IN_DIR}/*.MP3") + glob.glob(f"{AUDIO_IN_DIR}/*.mp3")
 text_paths = glob.glob(f"{TEXT_OUT_DIR}/*.md")
+print(f"\nStarting transcript generation.")
 for audio_path in audio_paths:
-    audio_filename = re.match(r"[a-zA-Z0-9-_\/\\]*[\/\\]([a-zA-Z0-9-_\/\\]*)\.(mp3|MP3)", audio_path).group(1)
+    audio_file_number = get_number_str_from_path(audio_path)
     text_file_exists = False
     for text_path in text_paths:
-        if audio_filename in text_path:
+        text_file_number = get_number_str_from_path(text_path)
+        if text_file_number == audio_file_number:
             text_file_exists = True
     if not text_file_exists:
-        print(f"Generating transcript for: {audio_filename}.MP3")
+        print(f"Generating transcript for audio file {audio_path}.")
 
         last_segment = ""
         text_segments = []
@@ -32,6 +38,6 @@ for audio_path in audio_paths:
             last_segment = segment.text
         text = "".join(text_segments)
 
-        with open(f"{TEXT_OUT_DIR}/{audio_filename}.md", "w") as file:
+        with open(f"{TEXT_OUT_DIR}/T{audio_file_number}_.md", "w") as file:
             file.write(text)
 print("\nDone!")
